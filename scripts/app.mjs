39dfde2767b62499edd4050aeae0f5a32c7c6aad
edstotest <65598531+edstotest@users.noreply.github.com>
2025-06-09 18:05:27 +0200

    Update app.mjs

--- scripts/app.mjs
+++ scripts/app.mjs
@@ -1,56 +1,56 @@
 import {
-    SNSClient,
-    PublishCommand
+  SNSClient,
+  PublishCommand
 } from "@aws-sdk/client-sns";
 
 const TOPIC_ARN_NAME = "arn:aws:sns:us-east-1:779846803720:ci-cd-UploadsNotificationTopic";
 const REGION = "us-east-1";
 
 const sns = new SNSClient({
-    region: REGION
+  region: REGION
 });
 
 export const handler = async (event, context) => {
-	if (!event) {
-		return {
-			'statusCode': 200,
-			'body': JSON.stringify('No message to process. Lambda function completed')
-		};
-	}
-    console.log(event.Records);
-    
-    const processed = await processRecords(event.Records);
-	
-	console.log(`SNS TOPIC ARN = ${TOPIC_ARN_NAME};
-	            Function Name = ${context.functionName};
-				Processed Message count = ${processed};
-				Remaining Time in millis = ${context.getRemainingTimeInMillis()}
-			  `);
-	return {
-		'statusCode': 200,
-		'body': JSON.stringify('Lambda function completed')
-    };
+if (!event) {
+  return {
+    'statusCode': 200,
+    'body': JSON.stringify('No message to process. Lambda function completed')
+  };
+}
+  console.log(event.Records);
+  
+  const processed = await processRecords(event.Records);
+
+console.log(`SNS TOPIC ARN = ${TOPIC_ARN_NAME};
+            Function Name = ${context.functionName};
+      Processed Message count = ${processed};
+      Remaining Time in millis = ${context.getRemainingTimeInMillis()}
+      `);
+return {
+  'statusCode': 200,
+  'body': JSON.stringify('Lambda function completed')
+  };
 };
 
 const processRecords = async (records) => {
-	if (!records) {
-		console.log("No records to process.");
-		return 0;
-	}
-	
-	for (const record of records) {
-		if (!record.body) {
-			throw new Error('No body in SQS record.');
-		}
-		await sns.send(new PublishCommand({
-			TopicArn: TOPIC_ARN_NAME,
-			Subject: "Processed SQS Queue Messages",
-			Message: record.body
-		}));
-		
-		console.log(`Message ${record.body} processed successfully.`);
-	}
-	
-	return records.length;
+if (!records) {
+  console.log("No records to process.");
+  return 0;
+}
+
+for (const record of records) {
+  if (!record.body) {
+    throw new Error('No body in SQS record.');
+  }
+  await sns.send(new PublishCommand({
+    TopicArn: TOPIC_ARN_NAME,
+    Subject: "Processed SQS Queue Messages",
+    Message: record.body
+  }));
+  
+  console.log(`Message ${record.body} processed successfully.`);
+}
+
+return records.length;
 };
 
